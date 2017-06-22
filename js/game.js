e360a5fb5cc7e9df4ab0ab259de58fff872efebc
christophszcz <christoph.szczechowicz@gmail.com>
2017-06-22 15:28:13 -0400

    Reorganize the JS files.

---
+++ js/game.js
@@ -0,0 +1,9 @@
+var colors = ['red', 'yellow', 'blue', 'yellow'], number, collection = [];
+ 
+function randomNumber(){
+	number = Math.floor(Math.random() * 4);
+	console.log(randomNumber);
+}
+
+randomNumber();
+console.log(number);
