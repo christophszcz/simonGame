da4b9911367222a16760d748a8ea17ab2adc2e41
christophszcz <christoph.szczechowicz@gmail.com>
2017-06-22 11:27:00 -0400

    Create a strict mode toggle.

--- js/script.js
+++ js/script.js
@@ -1,9 +1,27 @@
+var onOff = false;
+var strictMode = false;
+
 //On-off toggle
 var square = document.getElementById('square-id');
-square.addEventListener('click', function toggleSetting(){
+square.addEventListener('click', function(){
 	if(square.style.marginLeft === '0px' ||  square.style.marginLeft === ''){
 		square.style.marginLeft = '15px';
-	} else if (square.style.marginLeft === '15px'){
+		onOff = true;
+	} else if (square.style.marginLeft === '15px' && onOff === true){
 		square.style.marginLeft = '0px';
+		onOff = false;
+	}
+});
+
+//Strict mode toggle
+var strict = document.getElementById('strict-id');
+var dot = document.getElementById('dot-id');
+strict.addEventListener('click', function(){
+	if(onOff === true  && (dot.style.backgroundColor === "rgb(91, 68, 72)" || dot.style.backgroundColor === '') ){
+		dot.style.backgroundColor = '#DC0D29';
+		strictMode = true;
+	}else if (onOff === true && dot.style.backgroundColor === "rgb(220, 13, 41)"){
+		dot.style.backgroundColor = '#5B4448';
+		strictMode = false;
 	}
 });
