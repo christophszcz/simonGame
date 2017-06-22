c501eb2c248167f5253a907387c5a5c65df7b470
christophszcz <christoph.szczechowicz@gmail.com>
2017-06-22 11:56:41 -0400

    Change the count display into a form input.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,12 @@ var strictMode = false;
 
 //On-off toggle
 var square = document.getElementById('square-id');
+var count = document.getElementById('count-id');
 square.addEventListener('click', function(){
 	if(square.style.marginLeft === '0px' ||  square.style.marginLeft === ''){
 		square.style.marginLeft = '15px';
 		onOff = true;
+		count.value = '- -';
 	} else if (square.style.marginLeft === '15px' && onOff === true){
 		square.style.marginLeft = '0px';
 		onOff = false;
