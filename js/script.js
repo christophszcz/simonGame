f480ce4a07b5b24c84c656de21a35367daed9556
christophszcz <christoph.szczechowicz@gmail.com>
2017-06-22 10:46:08 -0400

    Add an on-off toggle.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,9 @@
-$(document).ready(function(){
-
+//On-off toggle
+var square = document.getElementById('square-id');
+square.addEventListener('click', function toggleSetting(){
+	if(square.style.marginLeft === '0px' ||  square.style.marginLeft === ''){
+		square.style.marginLeft = '15px';
+	} else if (square.style.marginLeft === '15px'){
+		square.style.marginLeft = '0px';
+	}
 });
