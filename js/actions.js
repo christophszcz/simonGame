e77733ab4d84f9d6140fbeece14a65acbb861ede
Chris Szczechowicz <info@cjszczechowicz.com>
2021-04-07 22:52:30 -0400

    Remove white spaces

--- js/actions.js
+++ js/actions.js
@@ -85,7 +85,7 @@ function toggleSounds(){
 		redSound.src = "https://s3.amazonaws.com/freecodecamp/simonSound1.mp3";
 		blueSound.src = "https://s3.amazonaws.com/freecodecamp/simonSound2.mp3";
 		yellowSound.src = "https://s3.amazonaws.com/freecodecamp/simonSound3.mp3";
-		greenSound.src = "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3"; 
+		greenSound.src = "https://s3.amazonaws.com/freecodecamp/simonSound4.mp3";
 	}
 	if (onOff === true){
 		red.addEventListener("click", function(){
